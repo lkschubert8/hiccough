a85152a557015a09032d1f4e7841268ed7df7d0f
Luke Schubert <lkschubert8@gmail.com>
2021-12-18 00:33:32 -0500

    Testing cli, and adding shorthand

--- src/hiccough/__init__.py
+++ src/hiccough/__init__.py
@@ -22,16 +22,18 @@ def html(val: Union[str, List]):
     if len(tag_sub) > 1:
         tag = tag_sub[0]
         if attr and "class" in attr.keys():
-            attr["class"] += " ".join(tag_sub[1:])
+            attr["class"] += " " + " ".join(tag_sub[1:])
         else:
             attr["class"] = " ".join(tag_sub[1:])
-
+    tag, id, *_ = pad_list_none(tag.split("#"), 3)
+    print(tag)
+    if id:
+        attr["id"] = id
     body = "" if not len(val) >= 2 or not val[1] else html(val[1])
     if rest:
         body = html([">".join(rest), body])
-    if attr:
-        for key, val in attr.items():
-            attributes += f' {key}="{val}"'
+    for key, val in attr.items():
+        attributes += f' {key}="{val}"'
     return f"<{tag}{attributes}>{body}</{tag}>"
 
 
@@ -42,13 +44,16 @@ def pad_list_none(list: List, pad_size: int):
     return list + [None] * (pad_size - len(list))
 
 
-def main() -> int:
+def main(args) -> int:
     """Echo the input arguments to standard output"""
-    arg = eval(sys.argv[1])
+    if len(args) == 1:
+        print("Missing second arg for generating html", file=sys.stderr)
+        return 1
 
+    arg = eval(args[1])
     print(html(arg))
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # next section explains the use of sys.exit
+    sys.exit(main(sys.arg))  # next section explains the use of sys.exit
--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,8 @@
+from unittest.mock import MagicMock
+
 from hiccough import html
+
+import hiccough
 
 
 def test_simple_div():
@@ -19,3 +23,34 @@ def test_nesting_and_class():
         )
         == '<div class="container"><div class="section"><div class="box"><form><input type="checkbox"></input></form></div></div></div>'
     )
+
+
+def test_multi_class():
+    assert html(["div.a.b"]) == '<div class="a b"></div>'
+
+
+def test_class_merging():
+    assert (
+        html(["div.class-a", None, {"class": "class-b"}])
+        == '<div class="class-b class-a"></div>'
+    )
+
+
+def test_id_shorthand():
+    assert html(["div#name"]) == '<div id="name"></div>'
+
+
+def test_multi_id():
+    assert html(["div#name#blah"]) == '<div id="name"></div>'
+
+
+def test_main():
+    hiccough.html = MagicMock(return_value="")
+    assert hiccough.main(["hiccough", '["div"]']) == 0
+    hiccough.html.assert_called_with(["div"])
+
+
+def test_main_missing_arg():
+    hiccough.html = MagicMock(return_value="")
+    assert hiccough.main(["hiccough"]) == 1
+    hiccough.html.assert_not_called()
